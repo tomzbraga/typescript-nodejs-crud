43d4e247e7310371116dcb7ee17d5d6b43e4772d
Lucas <tomzbraga@gmail.com>
2026-07-08 01:24:42 -0300

    configura o NextJS para o frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Task Manager",
+  description: "Task manager app to help with daily routine.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <div className="min-h-dvh bg-black text-white">
+        <div className="mx-auto flex min-h-dvh w-full flex-col">
+          <main>{children}</main>
+        </div>
+      </div>
+    </html>
+  );
+}
